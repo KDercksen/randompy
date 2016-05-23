26b497a70c4aa5834b9d6e40848e9b2bdceb78ab
KDercksen <mail@koendercksen.com>
2016-05-23 11:25:21 +0200

    Implement blobs/uuids. Refactored build_request method. Refactored config fieldnames for easier use.

--- randompy.py
+++ randompy.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from io import StringIO
-from pprint import pprint
 from random import randint
 import argparse
 import configparser
@@ -18,21 +17,39 @@ METHODS = {
     'decimals': 'generateSignedDecimalFractions',
     'gaussians': 'generateSignedGaussians',
     'strings': 'generateSignedStrings',
+    'uuids': 'generateSignedUUIDs',
+    'blobs': 'generateSignedBlobs',
     'verify': 'verifySignature',
+}
+
+
+KEYS = {
+    'integers': ('min', 'max', 'replacement', 'base'),
+    'decimals': ('decimalPlaces', 'replacement'),
+    'gaussians': ('mean', 'standardDeviation', 'significantDigits'),
+    'strings': ('length', 'characters', 'replacement'),
+    'uuids': (),
+    'blobs': ('size', 'format'),
+    'verify': ('random', 'signature'),
 }
 
 # Default alphabets
 ABCS = {
-    "lower": string.ascii_lowercase,
-    "upper": string.ascii_uppercase,
-    "letters": string.ascii_letters,
-    "digits": string.digits,
-    "hexdigits": string.hexdigits,
-    "octdigits": string.octdigits,
-    "punctuation": string.punctuation,
-    "printable": string.printable,
-    "whitespace": string.whitespace
+    'lower': string.ascii_lowercase,
+    'upper': string.ascii_uppercase,
+    'letters': string.ascii_letters,
+    'digits': string.digits,
+    'hexdigits': string.hexdigits,
+    'octdigits': string.octdigits,
+    'punctuation': string.punctuation,
+    'printable': string.printable,
+    'whitespace': string.whitespace
 }
+
+BLOB_FORMATS = [
+    'base64',
+    'hex',
+]
 
 
 def build_request(key, **kwargs):
@@ -45,29 +62,11 @@ def build_request(key, **kwargs):
     }
     if r['method'] != METHODS['verify']:
         r['params']['apiKey'] = key
-    if r['method'] == METHODS['integers']:
         r['params']['n'] = kwargs['number']
-        r['params']['min'] = kwargs['minimum']
-        r['params']['max'] = kwargs['maximum']
-        r['params']['replacement'] = kwargs['replacement']
-        r['params']['base'] = kwargs['base']
-    elif r['method'] == METHODS['decimals']:
-        r['params']['n'] = kwargs['number']
-        r['params']['decimalPlaces'] = kwargs['decimals']
-        r['params']['replacement'] = kwargs['replacement']
-    elif r['method'] == METHODS['gaussians']:
-        r['params']['n'] = kwargs['number']
-        r['params']['mean'] = kwargs['mean']
-        r['params']['standardDeviation'] = kwargs['stddev']
-        r['params']['significantDigits'] = kwargs['significant']
-    elif r['method'] == METHODS['strings']:
-        r['params']['n'] = kwargs['number']
-        r['params']['length'] = kwargs['length']
-        chars = ''.join(ABCS[a] for a in kwargs['chars'])
-        r['params']['characters'] = chars
-    elif r['method'] == METHODS['verify']:
-        r['params']['random'] = kwargs['random']
-        r['params']['signature'] = kwargs['signature']
+
+    for k in KEYS[kwargs['which']]:
+        r['params'][k] = kwargs[k]
+
     return rid, json.dumps(r)
 
 
@@ -141,7 +140,7 @@ def handle(config, **kwargs):
     print(output)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('defaults.ini')
     try:
@@ -149,84 +148,101 @@ if __name__ == "__main__":
     except:
         raise Exception('No user config found!')
 
-    parser = argparse.ArgumentParser(description="Get random numbers!")
+    parser = argparse.ArgumentParser(description='Get random numbers!')
     subparsers = parser.add_subparsers()
 
     # Add subparsers for the different methods.
-    parser_int = subparsers.add_parser("integers")
-    parser_dec = subparsers.add_parser("decimals")
-    parser_gau = subparsers.add_parser("gaussians")
-    parser_str = subparsers.add_parser("strings")
+    parser_int = subparsers.add_parser('integers')
+    parser_dec = subparsers.add_parser('decimals')
+    parser_gau = subparsers.add_parser('gaussians')
+    parser_str = subparsers.add_parser('strings')
+    parser_uui = subparsers.add_parser('uuids')
+    parser_blo = subparsers.add_parser('blobs')
 
     # Every subparser needs this argument so it's for the parent parser.
     number = int(config['root']['number'])
-    idfile = config['config']['path']
 
-    parser.add_argument("-n", "--number", type=int, default=number,
-                        help="number of randoms to generate (range 1:1000)")
+    parser.add_argument('-n', '--number', type=int, default=number,
+                        help='number of randoms to generate (range 1:1000)')
 
     # Add integer arguments
     integer = config['integer']
-    minimum = int(integer['minimum'])
-    maximum = int(integer['maximum'])
+    minimum = int(integer['min'])
+    maximum = int(integer['max'])
     replacement = config.getboolean('integer', 'replacement')
     base = int(integer['base'])
 
-    parser_int.add_argument("-m", "--minimum", type=int, default=minimum,
-                            help="minimum of random numbers (range -1e9:1e9)")
-    parser_int.add_argument("-M", "--maximum", type=int, default=maximum,
-                            help="maximum of random numbers (range -1e9:1e9)")
-    parser_int.add_argument("-r", "--replacement", action="store_false",
+    parser_int.add_argument('-m', '--min', type=int, default=minimum,
+                            help='minimum of random numbers (range -1e9:1e9)')
+    parser_int.add_argument('-M', '--max', type=int, default=maximum,
+                            help='maximum of random numbers (range -1e9:1e9)')
+    parser_int.add_argument('-r', '--replacement', action='store_false',
                             default=replacement,
-                            help="pick without replacement")
-    parser_int.add_argument("-b", "--base", type=int, choices=[2, 8, 10, 16],
-                            default=base, help="base to display numbers in")
-    parser_int.set_defaults(which="integers")
+                            help='pick without replacement')
+    parser_int.add_argument('-b', '--base', type=int, choices=[2, 8, 10, 16],
+                            default=base, help='base to display numbers in')
+    parser_int.set_defaults(which='integers')
 
     # Add decimal fraction arguments
     decimal = config['decimal']
-    decimals = int(decimal['decimals'])
+    decs = int(decimal['decimalPlaces'])
     replacement = config.getboolean('decimal', 'replacement')
 
-    parser_dec.add_argument("-d", "--decimals", type=int, default=decimals,
-                            choices=range(1, 21), metavar="N",
-                            help="number of decimal places")
-    parser_dec.add_argument("-r", "--replacement", action="store_false",
+    parser_dec.add_argument('-d', '--decimalPlaces', type=int, default=decs,
+                            choices=range(1, 21), metavar='N',
+                            help='number of decimal places')
+    parser_dec.add_argument('-r', '--replacement', action='store_false',
                             default=replacement,
-                            help="pick without replacement")
-    parser_dec.set_defaults(which="decimals")
+                            help='pick without replacement')
+    parser_dec.set_defaults(which='decimals')
 
     # Add gaussian arguments
     gaussian = config['gaussian']
     mean = float(gaussian['mean'])
-    stddev = float(gaussian['stddev'])
-    significant = int(gaussian['significant'])
+    stddev = float(gaussian['standardDeviation'])
+    significant = int(gaussian['significantDigits'])
 
-    parser_gau.add_argument("-m", "--mean", type=float, default=mean,
-                            help="the mean of the distribution")
-    parser_gau.add_argument("-s", "--stddev", type=float, default=stddev,
-                            help="the standard deviation of the distribution")
-    parser_gau.add_argument("-d", "--significant", type=int, metavar="N",
+    parser_gau.add_argument('-m', '--mean', type=float, default=mean,
+                            help='the mean of the distribution')
+    parser_gau.add_argument('-s', '--standardDeviation', type=float,
+                            help='the standard deviation of the distribution',
+                            default=stddev)
+    parser_gau.add_argument('-d', '--significantDigits', type=int, metavar='N',
                             default=significant, choices=range(2, 21),
-                            help="significant digits",)
-    parser_gau.set_defaults(which="gaussians")
+                            help='significant digits',)
+    parser_gau.set_defaults(which='gaussians')
 
     # Add string arguments
     strings = config['string']
     length = int(strings['length'])
-    chars = strings['chars']
+    chars = strings['characters']
     replacement = config.getboolean('string', 'replacement')
 
-    parser_str.add_argument("-l", "--length", type=int, choices=range(1, 21),
-                            default=length, help="length of strings",
-                            metavar="N")
-    parser_str.add_argument("-c", "--chars", metavar="string", nargs="+",
+    parser_str.add_argument('-l', '--length', type=int, choices=range(1, 21),
+                            default=length, help='length of strings',
+                            metavar='N')
+    parser_str.add_argument('-c', '--characters', metavar='string', nargs='+',
                             choices=ABCS.keys(), type=str, default=[chars],
-                            help="allowed alphabet (max length 80)")
-    parser_str.add_argument("-r", "--replacement", action="store_false",
+                            help='allowed alphabet (max length 80)')
+    parser_str.add_argument('-r', '--replacement', action='store_false',
                             default=replacement,
-                            help="pick without replacement")
-    parser_str.set_defaults(which="strings")
+                            help='pick without replacement')
+    parser_str.set_defaults(which='strings')
+
+    # Add uuids arguments
+    parser_uui.set_defaults(which='uuids')
+
+    # Add blobs arguments
+    size = int(config['blob']['size'])
+    form = config['blob']['format']
+
+    parser_blo.add_argument('-s', '--size', type=int, default=size,
+                            help='size of each blob measured in bits \
+                                  (range 1:1048576, must be divisble by 8)')
+    parser_blo.add_argument('-f', '--format', type=str, default=form,
+                            choices=BLOB_FORMATS, help='specifies blob return \
+                            format')
+    parser_blo.set_defaults(which='blobs')
 
     args = parser.parse_args()
 
